69fa373ec10b64ec06ee4b8b80235529beeb2990
Sim Jing Jia <simjingjia2016@gmail.com>
2025-02-05 09:14:57 +0800

    starting login form

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import LoginForm from '@/components/auth/login-form'
+
+const Login = () => {
+  return (
+    <LoginForm/>
+  )
+}
+
+export default Login
---
+++ components/auth/login-form.tsx
@@ -0,0 +1,17 @@
+"use client"
+import { CardWrapper } from "@/components/auth/card-wrapper"
+
+const LoginForm = () => {
+  return (
+    <CardWrapper
+       headerLabel="Welcome Back"
+       backButtonLabel= "Don't have an account?"
+       backButtonHref="/auth/register"
+       showSocial
+    >
+        LoginForm!
+    </CardWrapper>
+  )
+}
+
+export default LoginForm
