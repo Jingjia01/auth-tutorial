24c25bafa76683e5517db1e24489a7243fb23372
Sim Jing Jia <simjingjia2016@gmail.com>
2025-02-12 13:36:26 +0800

    finished middleware

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,41 @@
-import GitHub from "next-auth/providers/github"
-import type { NextAuthConfig } from "next-auth"
- 
-export default { 
-    providers: [GitHub] 
-} satisfies NextAuthConfig
+import Credentials from "next-auth/providers/credentials";
+import type { NextAuthConfig } from "next-auth";
+import bcrypt from "bcryptjs";
+
+import { LoginSchema } from "./schemas";
+import { getUserByEmail } from "./data/user";
+
+export default {
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (!validatedFields.success) {
+          return null;
+        }
+
+        const { email, password } = validatedFields.data;
+        const user = await getUserByEmail(email);
+
+        if (!user || !user.password) {
+          return null;
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          emailVerified: user.emailVerified,
+          image: user.image,
+        };
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,43 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
+import {
+    DEFAULT_LOGIN_REDIRECT,
+    apiAuthPrefix,
+    authRoutes,
+    publicRoutes,
+} from "@/routes";
 
 const { auth } = NextAuth(authConfig);
 
-export default auth((req) => {
+export default auth((req, ctx) => {  
+    const { nextUrl } = req;
     const isLoggedIn = !!req.auth;
-    console.log("ROUTE: ", req.nextUrl.pathname);
-    console.log("IS LOGGED IN: ", isLoggedIn);
-})
+
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+    if (isApiAuthRoute) {
+        return undefined; 
+    }
+
+    if (isAuthRoute) {
+        if (isLoggedIn) {
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+        }
+        return undefined;
+    }
+
+    if(!isLoggedIn && !isPublicRoute){
+        return Response.redirect(new URL("/auth/login", nextUrl));
+    }
+
+    return undefined;
+});
 
 export const config = {
     matcher: [
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)', '/(api|trpc)(.*)',
-      ],
-}
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        '/(api|trpc)(.*)',
+    ],
+};
